7f852eb1268e040b28726f1b0b78e758543129e1
pasamvijay7 <96808128+pasamvijay7@users.noreply.github.com>
2026-02-19 23:12:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+function setGreeting() {
+  const hour = new Date().getHours();
+  const greetingElement = document.getElementById("greetingTitle");
+
+  if (hour < 12) {
+    greetingElement.innerText = "🌅 Good Morning";
+  } else if (hour < 17) {
+    greetingElement.innerText = "☀️ Good Afternoon";
+  } else if (hour < 21) {
+    greetingElement.innerText = "🌇 Good Evening";
+  } else {
+    greetingElement.innerText = "🌙 Good Night";
+  }
+}
+
+setGreeting();
+
 function showPromise() {
   const randomIndex = Math.floor(Math.random() * Cards.length);
   const verse = Cards[randomIndex];
@@ -16,4 +33,6 @@ function copyVerse() {
   const text = document.getElementById("promiseText").innerText;
   navigator.clipboard.writeText(text);
   alert("Verse copied!");
+
 }
+
